64880743a6f9963c8cbbd4b100b154c61e9d440d
backdfb <wva22lfr@gmail.com>
2024-03-12 13:41:27 +0900

    signform_update

---
+++ src/SignUpPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SignUpPage = () => {
+    return (
+      <div className="signup-container">
+        <h1 className="heading">Welcome to TaskList Pro</h1>
+        <div className="input-container">
+          <input
+            type="email"
+            placeholder="name@yourcompany.com"
+            className="email-input"
+          />
+          <button className="email-button">Continue with Email</button>
+        </div>
+        <div className="auth-options">
+          <button className="google-button">Continue with Google</button>
+          <button className="apple-button">Continue with Apple</button>
+        </div>
+        <div className="sign-in-link">
+          <a href="#">Already have an account? Sign In</a>
+        </div>
+      </div>
+    );
+  };
+
+export default SignUpPage
